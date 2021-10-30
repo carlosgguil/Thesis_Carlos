9e04f4bbce73227d3e008dbbc6156ba9fe429545
carlosgguil <60291129+carlosgguil@users.noreply.github.com>
2021-10-30 17:18:35 +0200

    Addition of JICF mesh increase analysis

--- FIGURES_generator_python/ch5_JICF_SPS/ch5_nelem_plot.py
+++ FIGURES_generator_python/ch5_JICF_SPS/ch5_nelem_plot.py
@@ -4,8 +4,10 @@ Created on Mon Aug  9 18:26:19 2021
 
 @author: d601630
 """
-
+from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
+import numpy as np
 import pandas as pd
 
 FFIG = 0.5
@@ -17,21 +19,37 @@ plt.rcParams['axes.titlesize']  = 80*FFIG
 plt.rcParams['legend.fontsize'] = 50*FFIG
 plt.rcParams['font.size'] = 50*FFIG
 plt.rcParams['lines.linewidth'] =  6*FFIG
+plt.rcParams['legend.framealpha'] = 1.0
 plt.rcParams['legend.loc']      = 'lower right'
 plt.rcParams['text.usetex'] = True
 
 figsize_ = (FFIG*26,FFIG*16)
 
-folder_manuscript='C:/Users/d601630/Documents/GitHub/Thesis_Carlos/part2_developments/figures_ch5_resolved_JICF/'
-folder = 'C:/Users/d601630/Desktop/Ongoing/JICF/nelem evolution/'
+folder_manuscript='C:/Users/Carlos Garcia/Documents/GitHub/Thesis_Carlos/part2_developments/figures_ch5_resolved_JICF/JICF_nelem_evolution/'
+folder = 'C:/Users/Carlos Garcia/Desktop/Ongoing/JICF/nelem evolution/'
 
 
 
+x_label_ = r'$t^{\prime}$'
+#y_label_ = '$\# ~\mathrm{elements} ~(10^6$)'
+y_label_ = '$N_\mathrm{elements} ~(10^6$)'
 
 
+#tau_ph_UG100 = 0.019
+#tau_ph_UG75  = 0.026
 
-tau_ph_UG100 = 0.019
-tau_ph_UG75  = 0.026
+tau_dr_UG75_DX10  = 0.2952
+tau_dr_UG75_DX20  = 0.4567
+tau_dr_UG100_DX10 = 0.2187
+tau_dr_UG100_DX20 = 0.3628
+
+
+'''
+tau_dr_UG75_DX10  = 1
+tau_dr_UG75_DX20  = 1
+tau_dr_UG100_DX10 = 1
+tau_dr_UG100_DX20 = 1
+'''
 
 #%% Read files
 
@@ -40,41 +58,211 @@ nelem_all_cases = []
 
 # UG100_DX20
 df = pd.read_csv(folder + 'nelem_UG100_dx20.csv')
-time_UG100_DX20  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_ph_UG100
+time_UG100_DX20  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_dr_UG100_DX20
 nelem_UG100_DX20 = df['nelem'].values/1e6
 
 # UG100_DX10
 df = pd.read_csv(folder + 'nelem_UG100_dx10.csv')
-time_UG100_DX10  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_ph_UG100
+time_UG100_DX10  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_dr_UG100_DX10
 nelem_UG100_DX10 = df['nelem'].values/1e6
 
 
 
 # UG75_DX20
 df = pd.read_csv(folder + 'nelem_UG75_dx20.csv')
-time_UG75_DX20  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_ph_UG75
+time_UG75_DX20  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_dr_UG75_DX20
 nelem_UG75_DX20 = df['nelem'].values/1e6
 
 # UG100_DX10
 df = pd.read_csv(folder + 'nelem_UG75_dx10.csv')
-time_UG75_DX10  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_ph_UG75
+time_UG75_DX10  = (df['total_time'].values - df.iloc[0]['total_time'])*1e3/tau_dr_UG75_DX10
 nelem_UG75_DX10 = df['nelem'].values/1e6
 
-#%% 
+#%% Obtain values to get slopes of linear region
 
+tp_cases  = [time_UG100_DX20 , time_UG100_DX10 , time_UG75_DX20 , time_UG75_DX10 ]
+nel_cases = [nelem_UG100_DX20, nelem_UG100_DX10, nelem_UG75_DX20, nelem_UG75_DX10]
+tp_lower = 0.6
+tp_upper = 0.7
+dt = tp_upper - tp_lower
+
+nel_lower = []
+nel_upper = []
+slopes_linear = []
+slopes_semilog = []
+intercepts_semilog = []
+for k in range(len(tp_cases)):
+    
+    tp_cases_current = tp_cases[k]
+    nel_cases_current  = nel_cases[k]
+
+    found_lower = False
+    found_upper = False
+    i = 0
+    while (not found_lower):
+        tp_i = tp_cases_current[i]
+        if tp_i >= tp_lower:
+            found_lower = True
+            nel_lower_case = nel_cases_current[i]
+        i += 1
+    nel_lower.append(nel_lower_case)
+    
+    i = 0
+    while (not found_upper):
+        tp_i = tp_cases_current[i]
+        if tp_i > tp_upper:
+            found_upper = True
+            nel_upper_case = nel_cases_current[i-1]
+        i += 1
+    nel_upper.append(nel_upper_case)
+    
+    m_linear = (nel_upper_case - nel_lower_case)/dt
+    m_semilog = (np.log(nel_upper_case) - np.log(nel_lower_case))/dt
+    n_semilog = nel_upper_case*np.exp(-1*m_semilog*tp_upper)
+    
+    slopes_linear.append(m_linear)
+    slopes_semilog.append(m_semilog)
+    intercepts_semilog.append(n_semilog)
+
+
+#%% 
+plt.rcParams['ytick.minor.visible'] = True
+
+# Full figure
+plt.figure(figsize=figsize_)
+plt.plot([1]*2,[0,1e4],'--k')
+plt.plot(time_UG100_DX20,nelem_UG100_DX20, label='$\mathrm{UG}100\_\mathrm{DX}20$')
+plt.plot(time_UG100_DX10,nelem_UG100_DX10, label='$\mathrm{UG}100\_\mathrm{DX}10$')
+plt.plot(time_UG75_DX20,nelem_UG75_DX20, label='$\mathrm{UG}75\_\mathrm{DX}20$')
+plt.plot(time_UG75_DX10,nelem_UG75_DX10, label='$\mathrm{UG}75\_\mathrm{DX}10$')
+plt.xlabel(x_label_)
+#plt.xlabel("$t$")
+plt.ylabel(y_label_)
+#plt.xlim(1e-1,11)
+plt.ylim(nelem_UG75_DX10[0],3e3)
+#plt.xscale('log')
+plt.yscale('log')
+plt.legend(loc='best')
+plt.grid(which='major',linestyle='-',linewidth=4*FFIG)
+plt.grid(which='minor',linestyle='--')
+plt.tight_layout()
+#plt.savefig(folder_manuscript + 'JICF_nelem_increase.eps',format='eps',dpi=1000)
+plt.savefig(folder_manuscript + 'JICF_nelem_increase.pdf',format='pdf')
+plt.show()
+plt.close()
+
+# Zoom in t' \in [0, 2]
+plt.figure(figsize=figsize_)
+ax = plt.gca()
+ax.add_patch(Rectangle((0.005, 65), 0.490, 50, fill=None, alpha=1, 
+                       linewidth=5*FFIG,color='black',zorder=1e6))
+ax.add_patch(Rectangle((0.501, 80), 0.2, 1.5e2, fill=None, alpha=1, 
+                       linewidth=5*FFIG,color='blue',zorder=1e6))
+plt.plot([1]*2,[0,1e4],'--k')
+plt.plot(time_UG100_DX20,nelem_UG100_DX20, label='$\mathrm{UG}100\_\mathrm{DX}20$')
+plt.plot(time_UG100_DX10,nelem_UG100_DX10, label='$\mathrm{UG}100\_\mathrm{DX}10$')
+plt.plot(time_UG75_DX20,nelem_UG75_DX20, label='$\mathrm{UG}75\_\mathrm{DX}20$')
+plt.plot(time_UG75_DX10,nelem_UG75_DX10, label='$\mathrm{UG}75\_\mathrm{DX}10$')
+#Rectangle((0.0,100),0.25,1e3,fill='k',alpha=1)
+plt.xlabel(x_label_)
+#plt.xlabel("$t$")
+plt.ylabel(y_label_)
+plt.xlim(0,2)
+plt.ylim(nelem_UG75_DX10[0],2e3)
+plt.yscale('log')
+plt.grid(which='major',linestyle='-',linewidth=4*FFIG)
+plt.grid(which='minor',linestyle='--')
+plt.tight_layout()
+plt.savefig(folder_manuscript + 'JICF_nelem_increase_t_in_0_2.pdf',format='pdf')
+plt.show()
+plt.close()
+
+
+# Zoom in t' \in [0, 0.5]
 plt.figure(figsize=figsize_)
 plt.plot(time_UG100_DX20,nelem_UG100_DX20, label='$\mathrm{UG}100\_\mathrm{DX}20$')
 plt.plot(time_UG100_DX10,nelem_UG100_DX10, label='$\mathrm{UG}100\_\mathrm{DX}10$')
 plt.plot(time_UG75_DX20,nelem_UG75_DX20, label='$\mathrm{UG}75\_\mathrm{DX}20$')
 plt.plot(time_UG75_DX10,nelem_UG75_DX10, label='$\mathrm{UG}75\_\mathrm{DX}10$')
-plt.xlabel('$t^*$')
-plt.ylabel('$\# ~\mathrm{elements} ~(10^6$)')
-plt.yscale('log')
-plt.legend(loc='best')
-plt.grid()
+plt.xlabel(x_label_)
+#plt.xlabel("$t$")
+plt.ylabel(y_label_)
+plt.xlim(0,0.5)
+plt.ylim(nelem_UG75_DX10[0],100)
+#plt.yscale('log')
+#plt.legend(loc='best')
+plt.grid(which='major',linestyle='-',linewidth=4*FFIG)
+#plt.grid(which='minor',linestyle='--')
 plt.tight_layout()
-plt.savefig(folder_manuscript + 'JICF_nelem_increase.eps',format='eps',dpi=1000)
+plt.savefig(folder_manuscript + 'JICF_nelem_increase_t_in_0_0p5.pdf',format='pdf')
 plt.show()
 plt.close()
+
+# Zoom in t' \in [0, 0.3]
+plt.figure(figsize=figsize_)
+plt.plot(time_UG100_DX20,nelem_UG100_DX20, label='$\mathrm{UG}100\_\mathrm{DX}20$')
+plt.plot(time_UG100_DX10,nelem_UG100_DX10, label='$\mathrm{UG}100\_\mathrm{DX}10$')
+plt.plot(time_UG75_DX20,nelem_UG75_DX20, label='$\mathrm{UG}75\_\mathrm{DX}20$')
+plt.plot(time_UG75_DX10,nelem_UG75_DX10, label='$\mathrm{UG}75\_\mathrm{DX}10$')
+plt.xlabel(x_label_)
+plt.ylabel(y_label_)
+plt.xlim(0,0.3)
+plt.ylim(nelem_UG75_DX10[0],72)
+#plt.yscale('log')
+#plt.legend(loc='best')
+plt.grid(which='major',linestyle='-',linewidth=4*FFIG)
+plt.grid(which='minor',linestyle='--')
+plt.tight_layout()
+plt.savefig(folder_manuscript + 'JICF_nelem_increase_t_in_0_0p3.pdf',format='pdf')
+plt.show()
+plt.close()
+
+
+#%% Zoom in linearly (logarithmically) evolving region
+
+#% Obtain slope lines
+# Intercept for slope lines
+m_dx10 = 3.15 ; n_dx10 = 22
+m_dx20 = 1.6  ; n_dx20 = 39 
+
+# Time values for slope lines
+tp_dx10 = np.array([0.555,0.595])
+tp_dx20 = np.array([0.605,0.645])
+
+# Slope lines
+nel_dx10 = n_dx10*np.exp(m_dx10*tp_dx10)
+nel_dx20 = n_dx20*np.exp(m_dx20*tp_dx20)
+
+
+plt.figure(figsize=figsize_)
+ax = plt.gca()
+t = ax.get_xaxis().get_major_ticks()
+for tick in ax.get_xaxis().get_major_ticks():
+    tick.set_pad(20.)
+plt.plot(tp_dx10, nel_dx10, '-.k')
+plt.plot(tp_dx20, nel_dx20, '-.k')
+plt.plot(time_UG100_DX20,nelem_UG100_DX20, label='$\mathrm{UG}100\_\mathrm{DX}20$')
+plt.plot(time_UG100_DX10,nelem_UG100_DX10, label='$\mathrm{UG}100\_\mathrm{DX}10$')
+plt.plot(time_UG75_DX20,nelem_UG75_DX20, label='$\mathrm{UG}75\_\mathrm{DX}20$')
+plt.plot(time_UG75_DX10,nelem_UG75_DX10, label='$\mathrm{UG}75\_\mathrm{DX}10$')
+plt.text(0.56,1.4e2,r'$m_{\Delta x10}$')
+plt.text(0.61,1.1e2,r'$m_{\Delta x20}$')
+plt.xlabel(x_label_)
+plt.ylabel(y_label_)
+plt.xlim(0.5,0.7)
+plt.ylim(80,2e2)
+#plt.xlim(0.5,1)
+#plt.ylim(80,4e2)
+plt.yscale('log')
+ax.yaxis.set_minor_locator(tck.FixedLocator([8e1,2e2]))
+plt.grid(which='major',linestyle='-',linewidth=4*FFIG)
+plt.grid(which='minor',linestyle='--')
+plt.tight_layout()
+plt.savefig(folder_manuscript + 'JICF_nelem_increase_t_in_0p5_1.pdf',format='pdf')
+plt.show()
+plt.close()
+
+
+
 
 
